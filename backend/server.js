235f392cd261beb09074b1d228c4b027001c1aa2
Manuel Rojas <manuerjs70@gmail.com>
2023-04-26 00:20:49 -0600

    update: implemented routes, configurations and middlewares for the server.js

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,57 @@
-console.log('hello world');
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const corsOptions = require('./config/corsOptions');
+const errorHandler = require('./middleware/errorHandler');
+const verifyJWT = require('./middleware/verifyJWT');
+const cookieParser = require('cookie-parser');
+const credentials = require('./middleware/credentials');
+const mongoose = require('mongoose');
+const connectDB = require('./config/dbConn');
+const PORT = process.env.PORT || 3500;
+
+// Connect to MongoDB
+connectDB();
+
+// Handle options credentials check - before CORS!
+// and fetch cookies credentials requirement
+app.use(credentials);
+
+// Cross Origin Resource Sharing
+app.use(cors(corsOptions));
+
+// built-in middleware to handle urlencoded form data
+app.use(express.urlencoded({ extended: false }));
+
+// built-in middleware for json 
+app.use(express.json());
+
+//middleware for cookies
+app.use(cookieParser());
+
+// routes
+// app.use('/', require('./routes/root'));
+app.use('api/register', require('./routes/register'));
+app.use('api/auth', require('./routes/auth'));
+app.use('api/refresh', require('./routes/refresh'));
+app.use('api/logout', require('./routes/logout'));
+// routes which has to be auth
+app.use(verifyJWT);
+app.use('api/users', require('./routes/api/users'));
+
+app.all('*', (req, res) => {
+    res.status(404);
+    if (req.accepts('json')) {
+        res.json({ "error": "404 Not Found" });
+    } else {
+        res.type('txt').send("404 Not Found");
+    }
+});
+
+app.use(errorHandler);
+
+mongoose.connection.once('open', () => {
+    console.log('Connected to MongoDB');
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+});
