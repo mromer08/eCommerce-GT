2088e1d9f865f5f69c89765e295153b50ab81384
Manuel Rojas <manuerjs70@gmail.com>
2023-04-27 23:58:41 -0600

    feat: add index page for the app

---
+++ frontend/src/pages/Index.jsx
@@ -0,0 +1,14 @@
+import NavBar from "../components/NavBar";
+import ProductsList from "../components/ProductsList";
+function Index() {
+  return (
+    <>
+      <NavBar></NavBar>
+      <ProductsList>
+
+      </ProductsList>
+    </>
+  );
+}
+
+export default Index;
