de07548c248d6dad09669d3af563fd5a31c5d5c9
Manuel Rojas <manuerjs70@gmail.com>
2023-05-09 11:57:22 -0600

    update: product form component UI

--- frontend/src/components/ProductForm.jsx
+++ frontend/src/components/ProductForm.jsx
@@ -1,8 +1,110 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-export default function ProductForm({edit}) {
-    
+function ProductForm() {
+  const [name, setName] = useState('');
+  const [description, setDescription] = useState('');
+  const handleSubmit = () => { };
+
   return (
-    <div>ProductForm</div>
+    <section className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+      <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+        <form onSubmit={handleSubmit} className="space-y-6">
+          <div className="space-y-12">
+            <div>
+              <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+                <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+                  Nuevo producto
+                </h2>
+              </div>
+
+              <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+                <div className="col-span-full">
+                  <label
+                    htmlFor="name"
+                    className="block text-sm font-medium leading-6 text-gray-900"
+                  >
+                    Nombre
+                  </label>
+                  <div className="mt-2">
+                    <input
+                      name="name"
+                      id="name"
+                      type="text"
+                      // ref={nameRef}
+                      autoComplete="off"
+                      onChange={(e) => setName(e.target.value)}
+                      value={name}
+                      className="block w-full rounded-md border-0 p-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    />
+                  </div>
+                </div>
+
+                <div className="sm:col-span-2 sm:col-start-1">
+                  <label htmlFor="price" className="block text-sm font-medium leading-6 text-gray-900">
+                    Precio
+                  </label>
+                  <div className="mt-2">
+                    <input
+                      type="number"
+                      name="price"
+                      id="price"
+                      autoComplete="off"
+                      className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    />
+                  </div>
+                </div>
+
+                <div className="sm:col-span-2">
+                  <label htmlFor="amount" className="block text-sm font-medium leading-6 text-gray-900">
+                    Cantidad
+                  </label>
+                  <div className="mt-2">
+                    <input
+                      type="number"
+                      name="amount"
+                      id="amount"
+                      autoComplete="off"
+                      className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    />
+                  </div>
+                </div>
+
+                <div className="sm:col-span-3">
+                  <label htmlFor="tag" className="block text-sm font-medium leading-6 text-gray-900">
+                    Categoria
+                  </label>
+                  <div className="mt-2">
+                    <select
+                      id="tag"
+                      name="tag"
+                      autoComplete="off"
+                      className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6"
+                    >
+                      <option>Tecnologia</option>
+                      <option>Hogar</option>
+                      <option>Academico</option>
+                      <option>Literatura</option>
+                      <option>Decoracion</option>
+                      <option>Otros</option>
+                    </select>
+                  </div>
+                </div>
+
+
+
+
+              </div>
+            </div>
+            <div>
+              <button className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                Crear producto
+              </button>
+            </div>
+          </div>
+        </form>
+      </div>
+    </section>
   )
 }
+
+export default ProductForm
